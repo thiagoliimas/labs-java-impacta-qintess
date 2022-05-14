4a45a014e66775fca9c43cd6096e4e9a43e0a2b2
Thiago Lima <98200923+thiagoliimas@users.noreply.github.com>
2022-05-14 11:58:25 -0300

    Add files via upload
    Lab__Cap1_Controladores

---
+++ Lab__Cap1_Controladores/SomandoDoisValoresEMostrandoOResultado.java
@@ -0,0 +1,13 @@
+package exemplo1;
+
+public class lab1_1 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		
+		int resultado = a+b;
+		
+		System.out.println(resultado);
+	}
+}
---
+++ Lab__Cap1_Controladores/VerificandoSeUmNumeroEParOuImpar.java
@@ -0,0 +1,11 @@
+package exemplo1;
+
+public class lab1_2 {
+
+	public static void main(String[] args) {
+		
+		int valor = 21;
+		
+		System.out.println(valor%2==0? "Par":"Ímpar");
+	}
+}
