e865bcddfb147fa139e1583e7ed2145d83f1d30a
Thiago Lima <98200923+thiagoliimas@users.noreply.github.com>
2022-05-26 22:23:24 -0300

    Add files via upload

---
+++ Lab_Cap2_ControleDeFluxo/ExibindoOsAnosDeOcorrênciaDeTodasAsCopasDoMundoDeFutebol.java
@@ -0,0 +1,15 @@
+package laboratórios;
+
+public class controleDeFluxo_lab3 {
+
+	public static void main(String[] args) {
+		
+		for(int ano = 1930; ano <= 2022; ano += 4) {
+			
+			if(ano == 1942 || ano == 1946) {
+				continue;
+			}
+			System.out.println("Compa do mundo de " + ano);
+		}
+	}
+}
