71096bdb250afc4b1f380c8267678d839cbff128
Thiago Lima <98200923+thiagoliimas@users.noreply.github.com>
2022-05-27 23:57:46 -0300

    Add files via upload

---
+++ Lab_Cap3_Calculadora/Calculadora.java
@@ -0,0 +1,26 @@
+package Exemplo.Metodo;
+
+public class Calculadora {
+	
+	public double somar(double valor1, double valor2) {
+		
+		double resultado = valor1 + valor2;
+		return resultado;
+	}
+		
+	public double subtrair(double valor1, double valor2) {
+		
+		double resultado = valor1 - valor2;
+		return resultado;
+	}
+	
+	public double dividir (double valor1, double valor2) {
+		double resultado = valor1 / valor2;
+		return resultado;
+	}
+	
+	public double multiplicar(double valor1, double valor2) {
+		double resultado = valor1 * valor2;
+		return resultado;
+	}
+}
